ba760259ef9f42009dead006ca0b9378ee7f6884
Tommislav <tommy@salomonsson.se>
2014-06-16 22:55:37 +0200

    renderRectangleSystem implementation went into the header

--- include/RenderRectanglesSystem.h
+++ include/RenderRectanglesSystem.h
@@ -10,13 +10,29 @@
 
 class RenderRectanglesSystem : public artemis::EntityProcessingSystem
 {
-	public:
-		RenderRectanglesSystem();
-	protected:
-		virtual void processEntity(artemis::Entity &e);
 	private:
 		artemis::ComponentMapper<RectangleComponent> rectangleMapper;
 		artemis::ComponentMapper<PositionComponent> positionMapper;
+
+	public:
+		RenderRectanglesSystem() {
+			addComponentType<RectangleComponent>();
+			addComponentType<PositionComponent>();
+		}
+
+		virtual void initialize() {
+			rectangleMapper.init(*world);
+			positionMapper.init(*world);
+		}
+
+	protected:
+		virtual void processEntity(artemis::Entity &e) {
+			RectangleComponent *r = rectangleMapper.get(e);
+			PositionComponent *p = positionMapper.get(e);
+			ofSetHexColor( r->color );
+			ofRect(p->posX, p->posY, r->width, r->height);
+		}
+
 };
 
 #endif // RENDERRECTANGLESSYSTEM_H
